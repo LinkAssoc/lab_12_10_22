e3319ee63b481a744d980e248707d5eb28ce158c
LinkAssoc <linklol228@gmail.com>
2022-10-21 02:55:56 +0300

    Added mkdir, rmdir, touch, rm, cat, vim and -rf

---
+++ lab_12_10_22/FileEditor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_12_10_22
+{
+    public class FileManager
+    {
+        public string FilePath { get; set; }
+
+        public FileManager(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("Файл с таким именем не существует.");
+            }
+            FilePath = filePath;
+        }
+
+        public bool EditFile()
+        {
+            ConsoleKeyInfo c;
+            using StreamReader sr = new StreamReader(FilePath);
+            string lines = "";
+            lines += sr.ReadToEnd();
+            sr.Close();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(lines);
+                c = Console.ReadKey(true);
+                if (c.Key == ConsoleKey.Enter)
+                {
+                    lines += Environment.NewLine;
+                }
+                else if (c.Key == ConsoleKey.Backspace && lines.Length > 0)
+                {
+                    lines = lines.Remove(lines.Length - 1, 1);
+                }
+                else if (c.Key != ConsoleKey.Escape)
+                {
+                    lines += c.KeyChar;
+                }
+            }
+            while (c.Key != ConsoleKey.Escape);
+            using StreamWriter sw = new StreamWriter(FilePath);
+            sw.WriteLine(lines);
+            return true;
+        }
+    }
+}
--- lab_12_10_22/Manager.cs
+++ lab_12_10_22/Manager.cs
@@ -14,11 +14,20 @@ namespace lab_12_10_22
 
     public class Manager
     {
-        private static DirectoryInfo Directory { get; set; } = new DirectoryInfo(Environment.CurrentDirectory);
+        private static DirectoryInfo CurrentDirectory { get; set; } = new DirectoryInfo(Environment.CurrentDirectory);
 
         public void Print()
         {
-            Console.WriteLine(Directory.FullName);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[{Environment.UserName}] {CurrentDirectory.FullName}");
+            Console.ResetColor();
+        }
+
+        public void PrintError(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ResetColor();
         }
 
         /*
@@ -49,23 +58,23 @@ namespace lab_12_10_22
                         throw new Exception("Не введен путь.");
                     if (path == "..")
                     {
-                        if (Directory.Parent == null)
+                        if (CurrentDirectory.Parent == null)
                         {
                             throw new Exception("Папки не существует.");
                         }
-                        Directory = Directory.Parent;
+                        CurrentDirectory = CurrentDirectory.Parent;
                     }
                     else
                     {
-                        var directory = new DirectoryInfo(Path.Combine(Directory.FullName, path));
+                        var directory = new DirectoryInfo(Path.Combine(CurrentDirectory.FullName, path));
                         var directory2 = new DirectoryInfo(path);
                         if (directory.Exists)
                         {
-                            Directory = directory;
+                            CurrentDirectory = directory;
                         }
                         else if (directory2.Exists)
                         {
-                            Directory = directory2;
+                            CurrentDirectory = directory2;
                         }
                         else if (path != ".")
                         {
@@ -75,27 +84,141 @@ namespace lab_12_10_22
                 }
                 else if (command == "ls")
                 {
-                    foreach (DirectoryInfo di in Directory.EnumerateDirectories())
+                    foreach (DirectoryInfo di in CurrentDirectory.EnumerateDirectories())
                     {
                         PrintDirectoryInfo(di);
                     }
-                    foreach (FileInfo fi in Directory.EnumerateFiles())
+                    foreach (FileInfo fi in CurrentDirectory.EnumerateFiles())
                     {
                         PrintFileInfo(fi);
                     }
+                }
+                else if (command == "clear")
+                {
+                    Clear();
+                }
+                else if (command == "mkdir")
+                {
+                    if (args.Length > 0)
+                    {
+                        string directoryPath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        if (Directory.Exists(directoryPath) && !args.Contains("-rf"))
+                        {
+                            throw new Exception("Папка с таким именем существует.");
+                        }
+                        if (Directory.Exists(directoryPath))
+                        {
+                            Directory.Delete(directoryPath, true);
+                        }
+                        new DirectoryInfo(directoryPath).Create();
+                    }
+                    else
+                        throw new Exception("Имя папки не введено.");
+                }
+                else if (command == "rmdir")
+                {
+                    if (args.Length > 0)
+                    {
+                        string directoryPath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        if (!Directory.Exists(directoryPath))
+                        {
+                            throw new Exception("Папки с таким именем не существует.");
+                        }
+                        var di = new DirectoryInfo(directoryPath);
+                        if ((di.GetDirectories().Length > 0 || di.GetFiles().Length > 0) && !args.Contains("-rf"))
+                        {
+                            throw new Exception("Папка не пуста.");
+                        }
+                        Directory.Delete(directoryPath, true);
+                    }
+                    else
+                        throw new Exception("Имя папки не введено.");
+                }
+                else if (command == "touch")
+                {
+                    if (args.Length > 0)
+                    {
+                        string filePath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        if (File.Exists(filePath) && !args.Contains("-rf"))
+                        {
+                            throw new Exception("Файл с таким именем существует.");
+                        }
+                        if (File.Exists(filePath))
+                        {
+                            File.Delete(filePath);
+                        }
+                        File.Create(filePath).Close();
+                    }
+                    else
+                        throw new Exception("Имя файла не введено.");
+                }
+                else if (command == "rm")
+                {
+                    if (args.Length > 0)
+                    {
+                        string filePath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        if (!File.Exists(filePath))
+                        {
+                            throw new Exception("Файл с таким именем не существует.");
+                        }
+                        var fi = new FileInfo(filePath);
+                        if (fi.Length > 0 && !args.Contains("-rf"))
+                        {
+                            throw new Exception("Файл не пуст.");
+                        }
+                        File.Delete(filePath);
+                    }
+                    else
+                        throw new Exception("Имя файла не введено.");
+                }
+                else if (command == "cat")
+                {
+                    if (args.Length > 0)
+                    {
+                        string filePath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        if (!File.Exists(filePath))
+                        {
+                            throw new Exception("Файл с таким именем не существует.");
+                        }
+                        using StreamReader stream = new StreamReader(filePath);
+                        while (!stream.EndOfStream)
+                        {
+                            Console.WriteLine(stream.ReadLine());               
+                        }
+                    }
+                    else
+                        throw new Exception("Имя файла не введено.");
+                }
+                else if (command == "vim")
+                {
+                    if (args.Length > 0)
+                    {
+                        string filePath = Path.Combine(CurrentDirectory.FullName, StringHelper.RemoveArguments(string.Join(" ", args)));
+                        var fileManager = new FileManager(filePath);
+                        fileManager.EditFile();
+                    }
+                    else
+                        throw new Exception("Имя файла не введено.");
                 }
                 else if (command == "exit")
                 {
                     return false;
                 }
                 Print();
-                return true;
+                
+
+                // здесь преобразовать в строку и вывод
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                PrintError("Нет доступа к папке или файлу.");
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return true;
+                PrintError(e.Message);
             }
+            return true;
         }
 
         void PrintDirectoryInfo(DirectoryInfo di)
@@ -112,22 +235,14 @@ namespace lab_12_10_22
         long GetDirectorySize(DirectoryInfo di)
         {
             long size = 0;
-            var currentDirectory = di;
-            foreach (FileInfo fi in currentDirectory.EnumerateFiles())
+            foreach (FileInfo fi in di.EnumerateFiles())
             {
                 size += fi.Length;
-            }
-            foreach (DirectoryInfo subDi in currentDirectory.EnumerateDirectories())
-            {
-                foreach (FileInfo fi in subDi.EnumerateFiles())
-                {
-                    size += fi.Length;
-                }
             }
             return size;
         }
 
-        enum Measure
+        enum FileMeasure
         {
             B,
             KB,
@@ -144,7 +259,12 @@ namespace lab_12_10_22
                 size /= 1024;
                 i++;
             }
-            return Math.Round(size, 3) + ((Measure)i).ToString();
+            return Math.Round(size, 3) + ((FileMeasure)i).ToString();
+        }
+
+        void Clear()
+        {
+            Console.Clear();
         }
     }
 }
